e130c87b2377109f74aedff111c170d6df6481c7
Abdessalam HOCINE <ahocine@e2r12p12.42.fr>
2021-07-26 05:04:45 +0200

    A

--- get_next_line.c
+++ get_next_line.c
@@ -22,7 +22,6 @@ void	ft_putstr(char *s)
 		write(1, &s[i], 1);
 		i++;
 	}
-	write(1, "\n\n", 2);
 }
 
 int	ft_search_end(char *str)
@@ -36,13 +35,12 @@ int	ft_search_end(char *str)
 	return (0);
 }
 
-void	ft_print_int(char *buff, ssize_t lu, int i, int eol)
+static void	ft_print_int(char *buff, ssize_t lu, int eol)
 {
 	write(1, "buff = ", 8);
 	ft_putstr(buff);
-	write(1, "i = ", 5);
-	ft_putnbr_fd(++i, 1);
-	write(1, " lu = ", 6);
+	write(1, "\n", 1);
+	write(1, "lu = ", 6);
 	ft_putnbr_fd(lu, 1);
 	write(1, " eol = ", 7);
 	ft_putnbr_fd(eol, 1);
@@ -53,33 +51,38 @@ char	*get_next_line(int fd)
 {
 	ssize_t		lu;
 	char		buff[BUFFER_SIZE + 1];
-	static char	*curr_line;
+	char		*curr_line;
 	int			eol;
-	int			i = 0;
+	static char	*tmp;
 
 	if (fd < 0 || read(fd, NULL, 0) < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	lu = 1;
 	curr_line = (char *)malloc(sizeof(char) * (39 + 1));
 	eol = 0;
-	while (eol == 0 && lu > 0)
+	while (!eol && lu > 0)
 	{
 		lu = read(fd, buff, BUFFER_SIZE);
 		eol = ft_search_end(buff);
-		ft_print_int(buff, lu, ++i, eol);
+		ft_print_int(buff, lu, eol);
 		if (lu < 1)
 		{
 			free(curr_line);
 			return (NULL);
 		}
 		if (eol && eol < lu)
+		{
 			buff[eol] = 0;
+			curr_line = ft_strcat(curr_line, buff);
+		}
 		else
+		{
 			buff[lu] = 0;
-		
-		curr_line = ft_strcat(&curr_line[eol], buff);
+			tmp = ft_strcat(tmp, buff);
+		}
 		write(1, "curr_line = ", 13);
 		ft_putstr(curr_line);
+		write(1, "\n", 1);
 	}
 	return (curr_line);
 }
