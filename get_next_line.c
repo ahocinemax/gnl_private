1160975a24963a376217f56a43b9ad681e33e4a5
ahocine <ahocine@student.42.fr>
2021-08-01 21:54:35 +0200

    A

--- get_next_line.c
+++ get_next_line.c
@@ -35,7 +35,7 @@ int	ft_search_end(char *str)
 	return (0);
 }
 
-static void	ft_print_int(char *buff, ssize_t lu, int eol)
+/*static void	ft_print_int(char *buff, ssize_t lu, int eol)
 {
 	write(1, "buff = ", 8);
 	ft_putstr(buff);
@@ -45,7 +45,12 @@ static void	ft_print_int(char *buff, ssize_t lu, int eol)
 	write(1, " eol = ", 7);
 	ft_putnbr_fd(eol, 1);
 	write(1, "\n", 1);
-}
+}*/
+
+/*void	ft_read_and_join(char **buff, char **curr_line)
+{
+	;
+}*/
 
 char	*get_next_line(int fd)
 {
@@ -58,32 +63,24 @@ char	*get_next_line(int fd)
 	if (fd < 0 || read(fd, NULL, 0) < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	lu = 1;
-	curr_line = (char *)malloc(sizeof(char) * (39 + 1));
+//	curr_line = (char *)calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!curr_line)
+		return (NULL);
 	eol = 0;
 	while (!eol && lu > 0)
 	{
 		lu = read(fd, buff, BUFFER_SIZE);
 		eol = ft_search_end(buff);
-		ft_print_int(buff, lu, eol);
 		if (lu < 1)
 		{
 			free(curr_line);
 			return (NULL);
 		}
-		if (eol && eol < lu)
-		{
-			buff[eol] = 0;
-			curr_line = ft_strcat(curr_line, buff);
-		}
-		else
-		{
-			buff[lu] = 0;
-			tmp = ft_strcat(tmp, buff);
-		}
-		write(1, "curr_line = ", 13);
-		ft_putstr(curr_line);
-		write(1, "\n", 1);
+		buff[lu] = 0;
+		tmp = ft_strcat(curr_line, buff);
 	}
+	free(buff);
+	
 	return (curr_line);
 }
 
@@ -91,6 +88,7 @@ int	main(void)
 {
 	int		fd;
 	char	*ret;
+	int		i = 1;
 
 	fd = open("test.txt", O_RDONLY);
 	if (fd < 0)
@@ -98,12 +96,11 @@ int	main(void)
 	else
 	{
 		ret = get_next_line(fd);
-		//printf("ret : %s\n", ret);
-		while (ret)
-		{
-			//ft_putstr(ret);
-			ret = get_next_line(fd);
+		printf("%d line : %s\n", i++, ret);
+		ret = get_next_line(fd);
+		printf("%d line : %s\n", i++, ret);
+		ret = get_next_line(fd);
+		printf("%d line : %s\n", i++, ret);
 		}
-	}
 	return (0);
 }
