ddab8e8ed099c6bd429514d96d3714f1068330ca
ahocine <ahocine@student.42.fr>
2021-09-09 06:55:12 +0200

    A

--- main.c
+++ main.c
@@ -12,61 +12,27 @@
 
 #include "get_next_line.h"
 
-static void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-static void	ft_putnbr(int n)
-{
-	if (n < 0)
-		ft_putchar('-');
-	if (n < 0)
-		n *= -1;
-	if (n < 10)
-		ft_putchar(n + '0');
-	else
-	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
-	}
-}
-
-static void	ft_putstr(char *s)
-{
-	int		i;
-
-	i = 0;
-	while (s[i])
-		ft_putchar(s[i++]);
-}
-
 int	main(void)
 {
-	char	*ret = NULL;
+	char	*ret;
 	int		fd;
 	int		i;
 
+	ret = NULL;
 	i = 1;
 	fd = open("test.txt", O_RDONLY);
 	if (fd < 0)
 		printf("\e[31mError: open failed\e[0m\n");
 	else
 	{
-		while ((ret = get_next_line(fd)))
+		ret = get_next_line(fd);
+		while (ret)
 		{
-			ft_putstr("****************************************************\nline ");
-			ft_putnbr(i++);
-			ft_putstr(" : [");
-			ft_putstr(ret);
-			ft_putstr("]\n****************************************************\n\n");
+			printf("line %d = [%s]\n", i++, ret);
 			if (ret)
 				free(ret);
-			ret = NULL;
+			ret = get_next_line(fd);
 		}
-		if (ret)
-			free(ret);
-		ret = NULL;
 		close(fd);
 	}
 	return (0);
